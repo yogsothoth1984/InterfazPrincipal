9790e1d648f66c16060f4c8aa626e6dca8fca643
yogsothoth1984 <42740581+yogsothoth1984@users.noreply.github.com>
2019-05-22 10:27:45 -0300

    22/5
    declaradas variables

--- 44-InterfazPrincipal.java
+++ 44-InterfazPrincipal.java
@@ -3,5 +3,13 @@ import java.awt.*;
 import java.awt.event.*;
 
 public class Principal extends JFrame implements ActionListener{
-
+	private JmenuBar mb;
+	private JMenu menuOpciones,menuCalcular,menuAcercaDe,menuColorFondo;
+	private JMenuItem miCalculo,miRojo,miNegro,miMorado,miElCreador,miSalir,miNuevo;
+	private JLabel labelLogo,labelBienvenido,labelTitle,labelNombre,labelAPaterno,labelAMaterno,
+					labelDepartamento,labelAntiguedad,labelResultado,labelFooter;
+	private JTextField txtNombreTrabajador,txtAPaternoTrabajador,txtAMaternoTrabajador;
+	private JComboBox comboDepartamento,comboAntiguedad;
+	private JScrollPane scrollPane1;
+	private JTextArea textArea1;
 }
