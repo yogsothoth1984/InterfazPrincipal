88f5d8de834d8456c7559b1b00cabd7145c98369
yogsothoth1984 <42740581+yogsothoth1984@users.noreply.github.com>
2019-05-23 10:32:46 -0300

    10:32

--- 44-InterfazPrincipal.java
+++ 44-InterfazPrincipal.java
@@ -12,4 +12,29 @@ public class Principal extends JFrame implements ActionListener{
 	private JComboBox comboDepartamento,comboAntiguedad;
 	private JScrollPane scrollPane1;
 	private JTextArea textArea1;
+
+	public Principal(){
+		setLayout(null);
+		setTitle("Pantalla Principal");
+		getContentPane().setBackground(new Color(255,0,0));
+		setIconImage(new ImageIcon(getClass().getResource("images/icon.png")).getImage());
+
+		mb=new JMenuBar();
+		ms.setBackground(new Color(255,0,0));
+		setJBMenuBar(mb);
+
+		menuOpciones=new JMenu("Opciones");
+		menuOpciones.setBackground(new Color(255,0,0));
+		menuOpciones.setFont(new Font("Andale Mono",1,14));
+		menuOpciones.setForeground(new Color(255,255,255));
+		mb.add(menuOpciones);
+
+		menuCalcular=new JMenu("Calcular");
+		menuCalcular.setBackground(new Color(255,0,0));
+		menuCalcular.setFont(new Font("Andale Mono",1,14));
+		menuCalcular.setForeground(new Color(255,255,255));
+		mb.add(menuCalcular);
+
+		menuAcercaDe=new JMenu("Acerca de");
+	}
 }
