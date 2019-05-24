26bc0a363d7c8af07294f2099b993d100ac89ce5
yogsothoth1984 <42740581+yogsothoth1984@users.noreply.github.com>
2019-05-24 10:14:52 -0300

    10:14

--- 44-InterfazPrincipal.java
+++ 44-InterfazPrincipal.java
@@ -36,5 +36,68 @@ public class Principal extends JFrame implements ActionListener{
 		mb.add(menuCalcular);
 
 		menuAcercaDe=new JMenu("Acerca de");
+		menuAcercaDe.setBackground(new Color(255,0,0));
+		menuAcercaDe.setFont(new Font("Andale Mono",1,14));
+		menuAcercaDe.setForeground(new Color(255,0,255));
+		mb.add(menuAcercaDe);
+
+		menuColorFondo=new JMenu("Color de fondo");
+		menuColorFondo.setFont(new Font("Andale Mono",1,14));
+		menuColorFondo.setForeground(new Color(255,0,0));
+		menuOpciones.add(menuColorFondo);
+
+		miCalculo=new JMenuItem("Vacaciones");
+		miCalculo.setFont(new Font("Andale Mono",1,14));
+		miCalculo.setForeground(new Color(255,0,0));
+		menuCalcular.add(miCalculo);
+		miCalculo.addActionListener(this);
+
+		miRojo=new JMenuItem("Rojo");
+		miRojo.setFont(new Font("Andale Mono",1,14));
+		miRojo.setForeground(new Color(255,0,0));
+		menuColorFondo.add(miRojo);
+		miRojo.addActionListener(this);
+
+		miNegro=new JMenuItem("Negro");
+		miNegro.setFont(new Font("Andale Mono",1,14));
+		miNegro.setForeground(new Color(255,0,0));
+		menuColorFondo.add(miNegro);
+		miNegro.addActionListener(this);
+
+		miMorado=new JMenuItem("Morado");
+		miMorado.setFont(new Font("Andale Mono",1,14));
+		miMorado.setForeground(new Color(255,0,0));
+		menuColorFondo.add(miMorado);
+		miMorado.addActionListener(this);
+
+		miNuevo=new JMenuItem("Nuevo");
+		miNuevo.setFont(new Font("Andale Mono",1,14));
+		miNuevo.setForeground(new Color(255,0,0));
+		menuOpciones.add(miNuevo);
+		miNuevo.addActionListener(this);
+
+		miElCreador=new JMenuItem("ElCreador");
+		miElCreador.setFont(new Font("Andale Mono",1,14));
+		miElCreador.setForeground(new Color(255,0,0));
+		menuOpciones.add(miElCreador);
+		miElCreador.addActionListener(this);
+
+		miSalir=new JMenuItem("Salir");
+		miSalir.setFont(new Font("Andale Mono",1,14));
+		miSalir.setForeground(new Color(255,0,0));
+		menuOpciones.add(miSalir);
+		miSalir.addActionListener(this);
+
+		ImageIcon imagen=new ImageIcon("images/logo-coca.png");
+		labelLogo=new JLabel(imagen);
+		labelLogo.setBounds(5,5,250,100);
+		add(albelLogo);
+
+		labelBienvenido=new JLabel("Bienvenido");
+		labelBienvenido.setBounds(280,30,300,50);
+		labelBienvenido.setFont(new Font("Andale Mono",1,32));
+		labelBienvenido.setForeground(new Color(255,255,255));
+		add(labelBienvenido);
+
 	}
 }
